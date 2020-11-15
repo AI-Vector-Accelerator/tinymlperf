4f7e0dd33f0e8e897d2437fa30b4382ebbe844a0
pa4g17 <pa4g17@soton.ac.uk>
2020-11-15 10:53:00 +0000

    Change TF version to cv32e40p port
    Change the model of Tensorflow Lite for Microcontrol`lers used as the
    ML framework for the benchmark to a fork that supports the CV32E40P.
    
    As of this commit this port is hosted privately on a University of
    Southampton in-house hosting of GitLab. This will hopefully change after
    this project has been marked.

--- v0.1/example_submission/download_and_build_tfmicro.sh
+++ v0.1/example_submission/download_and_build_tfmicro.sh
@@ -1,12 +1,13 @@
 cd $(dirname $0)
 TFMICRO_BINARY=libtensorflow-microlite.a
 if [ ! -f "$TFMICRO_BINARY" ]; then
-  wget https://github.com/tensorflow/tensorflow/archive/master.zip
-  unzip -o master.zip
-  mv -f tensorflow-master/tensorflow .
-  rm -rf tensorflow-master
+  if [ ! -d "tensorflow" ]; then
+    curl --header "PRIVATE-TOKEN: Hb7KDihnd9UsPy3Vfymk" "https://git.soton.ac.uk/api/v4/projects/3745/repository/archive.zip" --output ./tensorflow-master.zip
+    unzip tensorflow-master.zip
+    mv -f tensorflow-master-*/tensorflow .
+    rm -rf tensorflow-master*
+  fi
   make -f tensorflow/lite/micro/tools/make/Makefile third_party_downloads
   make -f tensorflow/lite/micro/tools/make/Makefile microlite -j18
   mv tensorflow/lite/micro/tools/make/gen/*/lib/libtensorflow-microlite.a .
-  rm master.zip
 fi
