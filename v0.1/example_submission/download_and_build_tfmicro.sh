cd172042fe3e7c34ffb22cbdb3bc0fbf3b434243
pa4g17 <pa4g17@soton.ac.uk>
2020-11-26 10:00:43 +0000

    Add user-specified TF commit sha
    Add a variable to the TF download script to allow a user to specify
    a specific commit they want to test.

--- v0.1/example_submission/download_and_build_tfmicro.sh
+++ v0.1/example_submission/download_and_build_tfmicro.sh
@@ -1,11 +1,16 @@
+
+# Use this variable to set which commit of TensorFlow you would like to test.
+# This commit can be from any active branch.
+TF_COMMIT_SHA=d1b8c560c04d7e9e507dfc1dfe2faa648ac42878
+
 cd $(dirname $0)
 TFMICRO_BINARY=libtensorflow-microlite.a
 if [ ! -f "$TFMICRO_BINARY" ]; then
   if [ ! -d "tensorflow" ]; then
-    curl --header "PRIVATE-TOKEN: Hb7KDihnd9UsPy3Vfymk" "https://git.soton.ac.uk/api/v4/projects/3745/repository/archive.zip" --output ./tensorflow-master.zip
-    unzip tensorflow-master.zip
-    mv -f tensorflow-master-*/tensorflow .
-    rm -rf tensorflow-master*
+    curl --header "PRIVATE-TOKEN: Hb7KDihnd9UsPy3Vfymk" "https://git.soton.ac.uk/api/v4/projects/3745/repository/archive.zip?sha=$TF_COMMIT_SHA" --output ./tensorflow.zip
+    unzip tensorflow.zip
+    mv -f tensorflow-*/tensorflow .
+    rm -rf tensorflow-*
   fi
   make -f tensorflow/lite/micro/tools/make/Makefile third_party_downloads
   make -f tensorflow/lite/micro/tools/make/Makefile microlite $1 -j18
