23e976811f76f5f4ce4fb19b35be549307883978
Pete Alexader <peter.d.alexander23@gmail.com>
2021-01-13 13:47:36 +0000

    Set default TF branch to master if no commit SHA is given

--- v0.1/example_submission/download_and_build_tfmicro.sh
+++ v0.1/example_submission/download_and_build_tfmicro.sh
@@ -1,13 +1,18 @@
 
 # Use this variable to set which commit of TensorFlow you would like to test.
 # This commit can be from any active branch.
-TF_COMMIT_SHA=2f900c7ba883afeca5ee000be724cbd0e5252be5
+# If it is left blank then the most recent commit in the master branch will be pulled
+TF_COMMIT_SHA=
 
 cd $(dirname $0)
 TFMICRO_BINARY=libtensorflow-microlite.a
 if [ ! -f "$TFMICRO_BINARY" ]; then
   if [ ! -d "tensorflow" ]; then
-    curl --header "PRIVATE-TOKEN: Hb7KDihnd9UsPy3Vfymk" "https://git.soton.ac.uk/api/v4/projects/3745/repository/archive.zip?sha=$TF_COMMIT_SHA" --output ./tensorflow.zip
+    if [ ! -z "$TF_COMMIT_SHA" ]; then
+      curl --header "PRIVATE-TOKEN: Hb7KDihnd9UsPy3Vfymk" "https://git.soton.ac.uk/api/v4/projects/3745/repository/archive.zip?sha=$TF_COMMIT_SHA" --output ./tensorflow.zip
+    else
+      curl --header "PRIVATE-TOKEN: Hb7KDihnd9UsPy3Vfymk" "https://git.soton.ac.uk/api/v4/projects/3745/repository/archive.zip" --output ./tensorflow.zip
+    fi
     unzip tensorflow.zip
     mv -f tensorflow-*/tensorflow .
     rm -rf tensorflow-*
